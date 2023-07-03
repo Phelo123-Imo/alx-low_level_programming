cea8473f7114e0b724f2db0a622a3f20d588143c
Phelo123-Imo <ppalafini@gmail.com>
2023-07-03 11:02:03 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - draws a line of a given length
+ * @n: param
+ * Return: void
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n < 1)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar(' _ ');
+		}
+
+		_putchar('\n');
+	}
+}
